3d56f715177e4ca11751810f4aba0025751abb2b
Betrizav <anacarlafloressilva@gmail.com>
2023-09-18 23:04:16 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@ const { Sequelize } = require('sequelize');
 const config = require('../config/config')
 require('dotenv').config();
 
-const sequelize = new Sequelize(    );
+const sequelize = new Sequelize(  
+  "postgres://wgahajso:c9YemqzAKK5OIrxEUrdI5R4s09IDqwI1@motty.db.elephantsql.com/wgahajso" {
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  });
 
 try {
   sequelize.authenticate();
-  console.log('              ');
+  console.log('Conectado  com o elephantSQL!');
 } catch (error) {
-  console.error('             ', error);
+  console.error('Conexão falhou:', error);
 }
 
 module.exports = { Sequelize, sequelize };
