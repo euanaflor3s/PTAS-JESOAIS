cf87b10fc07ffee8d223a4cf8b7bbe303d93d7ee
euanaflor3s <anacarlafloressilva@gmail.com>
2023-11-16 15:54:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('./config/connection');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const bcrypt = require("bcryptjs");
 require('dotenv').config();
 
 const app = express();
@@ -16,7 +17,17 @@ const routes = require('./routers/routes');
 app.use(express.json(), routes, cors());
 app.listen(port, () => { console.log(`Run server...${port}`) });
 
+app.post("/cripto", async function cripto(req, res){
+    const { password } = req.body;
+    const newpassword = await bcrypt.hash(password, 10);
+    return res.send(newpassword);
+});
 
+app.post("/descrypt", async function descrypt(req, res) {
+    const { password, passwordCripto } = req.body;
+    const response = await bcrypt.compare(password, passwordCripto);
+    return res.send(response)
+}); 
 
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'index.html');
